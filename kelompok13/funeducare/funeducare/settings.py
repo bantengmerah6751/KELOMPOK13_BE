3dbae41176a5468d8902d5c7ea73b98149509d83
Dana Riyadi <fisikaekonomi6751@gmail.com>
2024-11-12 05:36:46 +0700

    tambahin app

--- kelompok13/funeducare/funeducare/settings.py
+++ kelompok13/funeducare/funeducare/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = BASE_DIR.joinpath("templates")
+STATIC_DIR = BASE_DIR.joinpath("static")
 
 
 # Quick-start development settings - unsuitable for production
@@ -42,6 +44,10 @@ INSTALLED_APPS = [
     'programs',
     'contactus',
     'pendaftaran',
+    'payments',
+    'profil',
+    'users',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +65,7 @@ ROOT_URLCONF = 'funeducare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR,]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
