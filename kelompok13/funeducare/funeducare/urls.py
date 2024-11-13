e562d1fdb7c42fe00ec8549d386e47a0933fd7b4
Zuhriya29 <113580042+Zuhriya29@users.noreply.github.com>
2024-11-13 10:41:01 +0700

    tambah file urls

--- kelompok13/funeducare/funeducare/urls.py
+++ kelompok13/funeducare/funeducare/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from aboutus import views
+from contactus import views
+from home import views
+from payments import views
+from pendaftaran import views
+from profil import views
+from programs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
