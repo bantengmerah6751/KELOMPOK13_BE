c5125b94173ed1beebdb66950d9fd946f87cd471
Dana R <danariyadi111@gmail.com>
2024-11-22 13:01:52 +0700

    Update models.py
    form booking

--- kelompok13/funeducare/pendaftaran/models.py
+++ kelompok13/funeducare/pendaftaran/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Pendaftaran(models.Model):
+    # Tambahkan field id dan created_at
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    
+    JENIS_KELAMIN_CHOICES = [
+        ('L', 'Laki-laki'),
+        ('P', 'Perempuan'),
+    ]
+
+    PROGRAM_CHOICES = [
+        ('infant', 'Infant Care (0-12 bulan)'),
+        ('toddler', 'Toddler Program (1-2 tahun)'),
+        ('preschool', 'Pre-school Program (3-4 tahun)'),
+        ('prekindergarten', 'Pre-kindergarten (4-5 tahun)'),
+    ]
+
+    nama_ortu = models.CharField(max_length=100, verbose_name="Nama Orang Tua")
+    email = models.EmailField(verbose_name="Alamat Email")
+    nomor_wa = models.CharField(max_length=15, verbose_name="Nomor WhatsApp Orang Tua")
+    alamat = models.TextField(verbose_name="Alamat Orang Tua")
+    ktp = models.FileField(upload_to='uploads/ktp/', verbose_name="Upload KTP Orang Tua")
+    nama_anak = models.CharField(max_length=100, verbose_name="Nama Anak")
+    umur_anak = models.CharField(max_length=2, verbose_name="Umur Anak")
+    jenis_kelamin = models.CharField(max_length=1, choices=JENIS_KELAMIN_CHOICES, verbose_name="Jenis Kelamin")
+    akta_kelahiran = models.FileField(upload_to='uploads/akta_kelahiran/', verbose_name="Upload Akta Kelahiran Anak")
+    program = models.CharField(max_length=15, choices=PROGRAM_CHOICES, verbose_name="Program")
+
+    def __str__(self):
+        return f"{self.nama_ortu} - {self.nama_anak}"
