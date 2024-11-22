ea0553a282f0d03769479caa5e1f1cc4e285c4a2
Dana R <danariyadi111@gmail.com>
2024-11-22 12:53:46 +0700

    Update urls.py
    tambahkan untuk atur booking

--- kelompok13/funeducare/funeducare/urls.py
+++ kelompok13/funeducare/funeducare/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 from aboutus import views
@@ -32,6 +35,7 @@ urlpatterns = [
     path('contactus/', include('contactus.urls')),
     path('aboutus/', include('aboutus.urls')),
     path('programs/', include('programs.urls')),
+    path('pendaftaran/', include('pendaftaran.urls')),
     path('users/', include('users.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
