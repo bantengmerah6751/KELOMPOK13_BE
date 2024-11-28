c6b193c9075745f57e417e073c8832f719d59e4d
Ndyy21 <zaendyana@gmail.com>
2024-11-28 18:35:10 +0700

    models.py

--- kelompok13/funeducare/aboutus/models.py
+++ kelompok13/funeducare/aboutus/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Galeri(models.Model):
+    nama = models.CharField(max_length=100)
+    foto = models.ImageField(upload_to='galeri_photos/', null=True, blank=True)
+    deskripsi_singkat = models.TextField()
+    deskripsi_detail = models.TextField()
+
+    def __str__(self):
+        return self.nama
+
+    class Meta:
+        verbose_name_plural = "Galeri"
