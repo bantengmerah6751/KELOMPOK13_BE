7fa8db8fa83f10a8798eb09e23b72c0bc24c4e22
Dana R <danariyadi111@gmail.com>
2024-12-08 19:59:46 +0700

    Update urls.py

--- kelompok13/funeducare/pendaftaran/urls.py
+++ kelompok13/funeducare/pendaftaran/urls.py
@@ -5,4 +5,5 @@ app_name = 'pendaftaran'
 urlpatterns = [
     path('', views.pendaftaran, name='pendaftaran'),  # Form pendaftaran
     path('syarat/', views.syarat, name='syarat'),
+    path('cara_mendaftar/', views.cara_mendaftar, name='cara_mendaftar'),
 ]
